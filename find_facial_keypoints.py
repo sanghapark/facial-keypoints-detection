acb5747713768141240e0a0f6a65a92ccc17da84
sanghapark <sanghapark1021@gmail.com>
2017-11-14 01:10:14 +0900

    fix lint

--- find_facial_keypoints.py
+++ find_facial_keypoints.py
@@ -233,7 +233,7 @@ for batch_index in range(1, int(np.ceil(X_test.shape[0]/batch_size)+1)):
     start = (batch_index-1)*batch_size
     end = batch_index*batch_size
     Y_predicted = m1.predict(X_test[start:end,], keep_prop=1.0)
-    print("Predicting {} ~ {} test images ({}, {})".format(start, end, len(X_test[start:end,], len(Y_predicted))))
+    print("Predicting {} ~ {} test images ({}, {})".format(start, end, len(X_test[start:end,]), len(Y_predicted)))
     partial_output = ud.batch_output_for_kaggle_submission(Y_predicted, batch_index, batch_size)
     total_output = pd.concat([total_output, partial_output])
 total_output.to_csv("./output/{}/kaggle_submission_CNN_TF.csv".format(datetime), index=0, columns = ['RowId','Location'] )
