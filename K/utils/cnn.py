8d60cc8814441c757b41e0a8e64d617a8505adde
sanghapark <sanghapark1021@gmail.com>
2017-11-17 04:00:18 +0900

    cnn3 elu tanh adam batchnorm glorotinit

--- K/utils/cnn.py
+++ K/utils/cnn.py
@@ -110,7 +110,8 @@ def create_cnn2(n_output, activation, last_activation):
 def create_cnn3(n_output, activation, last_activation):
     input_shape = (IMAGE_SIZE, IMAGE_SIZE, COLOR_CHANNEL)
     model = Sequential([
-        Convolution2D(128, 3, 3, border_mode='valid', input_shape=input_shape),
+        BatchNormalization(input_shape=input_shape),
+        Convolution2D(128, 3, 3, border_mode='valid', kernel_initializer='glorot_normal', input_shape=input_shape),
         Activation(activation),
         MaxPooling2D(pool_size=(2, 2)),
         Dropout(0.1),
