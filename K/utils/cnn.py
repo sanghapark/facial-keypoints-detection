adc3a736aa648f3723ef4ef8467bcff15e003516
sanghapark <sanghapark1021@gmail.com>
2017-11-17 00:18:28 +0900

    elu cnn2

--- K/utils/cnn.py
+++ K/utils/cnn.py
@@ -7,7 +7,9 @@ from utils.constant import *
 # Batch Normalization, Ensemble 참고
 # https://github.com/alexander-rakhlin/kaggle_otto/blob/master/keras.ensemble.py
 
-
+# 최고가 대략 4점대, 오래걸림
+# LeakyReLU: 별로다 처음에도 나아지질 않닸다. elu도 나아 지지 않는다.
+# relu가 가장 나았다.
 def create_cnn(n_output, activation, last_activation):
     input_shape = (IMAGE_SIZE, IMAGE_SIZE, COLOR_CHANNEL)
     model = Sequential()
@@ -40,36 +42,8 @@ def create_cnn(n_output, activation, last_activation):
     return model
 
 
-def create_cnn2(n_output, activation, last_activation):
-    input_shape = (IMAGE_SIZE, IMAGE_SIZE, COLOR_CHANNEL)
-    model = Sequential([
-        Convolution2D(128, 3, 3, border_mode='valid', input_shape=input_shape),
-        Activation(activation),
-        MaxPooling2D(pool_size=(2, 2)),
-        Dropout(0.1),
-
-        Convolution2D(256, 2, 2),
-        Activation(activation),
-        MaxPooling2D(pool_size=(2, 2)),
-        Dropout(0.25),
-
-        Convolution2D(512, 2, 2),
-        Activation(activation),
-        MaxPooling2D(pool_size=(2, 2)),
-        Dropout(0.25),
-
-        Flatten(),
-        Dense(512),
-        Activation(activation),
-        Dropout(0.5),
-        Dense(512),
-        Activation(last_activation),
-
-        Dense(n_output),
-    ])
-    print(model.summary())
-    return model
-
+# dataset01: min validation RMSE = 2.1115 (relu, tanh, adam)
+# dataset01: min validation RMSE = ?  (elu, tanh, adam)
 def create_cnn2(n_output, activation, last_activation):
     input_shape = (IMAGE_SIZE, IMAGE_SIZE, COLOR_CHANNEL)
     model = Sequential()
@@ -98,5 +72,36 @@ def create_cnn2(n_output, activation, last_activation):
     model.add(Dense(90, activation=last_activation))
     model.add(Dense(n_output))
 
+    print(model.summary())
+    return model
+
+
+def create_cnn3(n_output, activation, last_activation):
+    input_shape = (IMAGE_SIZE, IMAGE_SIZE, COLOR_CHANNEL)
+    model = Sequential([
+        Convolution2D(128, 3, 3, border_mode='valid', input_shape=input_shape),
+        Activation(activation),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.1),
+
+        Convolution2D(256, 2, 2),
+        Activation(activation),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Convolution2D(512, 2, 2),
+        Activation(activation),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Flatten(),
+        Dense(512),
+        Activation(activation),
+        Dropout(0.5),
+        Dense(512),
+        Activation(last_activation),
+
+        Dense(n_output),
+    ])
     print(model.summary())
     return model
