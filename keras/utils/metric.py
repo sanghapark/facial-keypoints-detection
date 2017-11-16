397d0b9a816254523524963a9ba1bdc001a6564d
sanghapark <sanghapark1021@gmail.com>
2017-11-16 18:21:05 +0900

    np.sqrt -> K.sqrt

--- keras/utils/metric.py
+++ keras/utils/metric.py
@@ -2,5 +2,5 @@ import numpy as np
 import keras.backend as K
 
 def rmse(target, y_predicted):
-    rmse = np.sqrt(K.mean(K.square(y_predicted - target))) * 48
+    rmse = K.sqrt(K.mean(K.square(y_predicted - target))) * 48
     return rmse
