0013352fb942806869bc26b9075f4f86bb5212f7
sanghapark <sanghapark1021@gmail.com>
2017-11-17 22:28:00 +0900

    callback 체크

--- K/utils/loss_history.py
+++ K/utils/loss_history.py
@@ -2,11 +2,12 @@ import pandas as pd
 from keras.callbacks import Callback
 
 class LossHistory(Callback):
-
     def on_training_begin(self, logs={}):
+        print('on_epoch_begin')
         self.losses = pd.DataFrame(columns=['loss', 'val_loss', 'rmse', 'val_rmse'])
 
     def on_batch_end(self, batch, logs=None):
+        print('on_epoch_begin')
         cols = ['loss', 'val_loss', 'rmse', 'val_rmse']
         metrics = [
             logs.get('loss'),
@@ -15,3 +16,16 @@ class LossHistory(Callback):
             logs.get('val_rmse')
         ]
         self.losses.append(pd.DataFrame(metrics, columns=cols))
+
+
+    def on_epoch_begin(self, epoch, logs=None):
+        print('on_epoch_begin')
+
+    def on_epoch_end(self, epoch, logs=None):
+        print('on_epoch_end')
+
+    def on_batch_begin(self, batch, logs=None):
+        print('on_batch_begin')
+
+    def on_train_end(self, logs=None):
+        print('on_train_end')
