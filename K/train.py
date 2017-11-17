49ea7073d95485e5a2af6181538d9b791d11933f
sanghapark <sanghapark1021@gmail.com>
2017-11-17 14:29:18 +0900

    formatting code

--- K/train.py
+++ K/train.py
@@ -10,7 +10,9 @@ from utils.constant import *
 
 
 def train(filepath, cols, flip_indices, optimizer, epochs):
-    X_train, X_valid, Y_train, Y_valid = load_train_data_and_split(FILEPATH_TRAIN, cols, VALIDATION_RATIO)
+    X_train, X_valid, Y_train, Y_valid = load_train_data_and_split(FILEPATH_TRAIN,
+                                                                   cols,
+                                                                   VALIDATION_RATIO)
     n_output = Y_train.shape[1]
     model = create_cnn2(n_output, ACTIVATION, LAST_ACTIVATION)
     checkpoint = ModelCheckpoint(filepath,
@@ -19,7 +21,7 @@ def train(filepath, cols, flip_indices, optimizer, epochs):
                                  save_best_only=True,
                                  save_weights_only=True,
                                  mode='min')
-    earlystopping = EarlyStopping(monitor='val_loss', 
+    earlystopping = EarlyStopping(monitor='val_loss',
                                   min_delta=0,
                                   patience=50,
                                   verbose=0,
@@ -33,12 +35,15 @@ def train(filepath, cols, flip_indices, optimizer, epochs):
                                      flip_ratio=FLIP_RATIO,
                                      rotate_ratio=ROTATE_RATIO,
                                      contrast_ratio=CONTRAST_RATIO)
-    model.fit_generator(generator.generate(batchsize=BATCH_SIZE, flip=FLIP, rotate=ROTATE, contrast=CONTRAST),
-                               steps_per_epoch=int(generator.size_train/BATCH_SIZE),
-                               epochs=epochs,
-                               verbose=1,
-                               callbacks=[checkpoint, earlystopping],
-                               validation_data=[X_valid, Y_valid])
+    model.fit_generator(generator.generate(batchsize=BATCH_SIZE,
+                                           flip=FLIP,
+                                           rotate=ROTATE,
+                                           contrast=CONTRAST),
+                        steps_per_epoch=int(generator.size_train/BATCH_SIZE),
+                        epochs=epochs,
+                        verbose=1,
+                        callbacks=[checkpoint, earlystopping],
+                        validation_data=[X_valid, Y_valid])
     model.save_weights(filepath)
     print('Weights are saved as {}'.format(filepath))
 
