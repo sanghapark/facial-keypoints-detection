8e72f9a540d74a68dd372a2ea37cb9f92aeb1402
sanghapark <sanghapark1021@gmail.com>
2017-11-18 00:26:31 +0900

    fix logging history

--- K/utils/loss_history.py
+++ K/utils/loss_history.py
@@ -3,16 +3,15 @@ from keras.callbacks import Callback
 
 class LossHistory(Callback):
 
-    def __init__(self):
-        self.losses = pd.DataFrame(columns=['epoch', 'loss', 'val_loss', 'rmse', 'val_rmse'])
+    def __init__(self, modelfile):
+        self.modelfile = modelfile
+        file = open(modelfile, 'a+')
+        file.write(','.join(['epoch', 'loss', 'val_loss', 'rmse', 'val_rmse']))
 
     def on_epoch_end(self, epoch, logs=None):
-        cols = ['epoch', 'loss', 'val_loss', 'rmse', 'val_rmse']
-        metrics = [[
-            epoch,
-            logs.get('loss'),
-            logs.get('val_loss'),
-            logs.get('rmse'),
-            logs.get('val_rmse')
-        ]]
-        self.losses.append(pd.DataFrame(metrics, columns=cols).copy())
+        file = open(self.modelfile, 'a+')
+        file.write(
+            ','.join(
+                [epoch, logs.get('loss'), logs.get('val_loss'), logs.get('rmse'), logs.get('val_rmse')]
+            )
+        )
