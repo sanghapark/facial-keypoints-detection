1cdacd805b199b854fc128c6807a14a60349d00d
sanghapark <sanghapark1021@gmail.com>
2017-11-17 12:19:00 +0900

    delete save_models.py

--- K/train.py
+++ K/train.py
@@ -1,4 +1,5 @@
 import os
+import datetime as dt
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense
 from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam
@@ -114,3 +115,14 @@ for i in range(20):
     filepath = 'cnn2_dataset02_{:02}.h5'.format(i)
     optimizer = Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     train(filepath, COLS02, FLIP_INDICES02, optimizer, EPOCHS02)
+
+
+# def save_model():
+#     model = create_cnn2(8, 'elu', 'tanh')
+#     optimizer = RMSprop(0.001, 0.9, 1e-8, 0)
+#     model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=[rmse])
+#     model.save('cnn2_dataset01.model')
+
+#     model_yaml = model.to_yaml()
+#     with open("cnn2_dataset01.yaml", "w") as yaml_file:
+#         yaml_file.write(model_yaml)
