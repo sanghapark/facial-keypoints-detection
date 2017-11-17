22a4dda5d200a99a3a072e2c012700766dfc940f
sanghapark <sanghapark1021@gmail.com>
2017-11-18 00:42:05 +0900

    파일 닫기

--- K/utils/loss_history.py
+++ K/utils/loss_history.py
@@ -4,11 +4,12 @@ from keras.callbacks import Callback
 class LossHistory(Callback):
 
     def __init__(self, modelfile):
-        self.modelfile = modelfile
-        file = open(modelfile, 'a+')
-        file.write(','.join(['epoch', 'loss', 'val_loss', 'rmse', 'val_rmse']))
+        self.file = open(modelfile, 'a+')
+        self.file.write(','.join(['epoch', 'loss', 'val_loss', 'rmse', 'val_rmse'])+'\n')
+
+    def __del__(self):
+        self.file.close()
 
     def on_epoch_end(self, epoch, logs=None):
-        file = open(self.modelfile, 'a+')
-        row = "{},{},{},{},{}".format(epoch, logs.get('loss'), logs.get('val_loss'), logs.get('rmse'), logs.get('val_rmse'))
-        file.write(row)
+        row = "{},{},{},{},{}\n".format(epoch, logs.get('loss'), logs.get('val_loss'), logs.get('rmse'), logs.get('val_rmse'))
+        self.file.write(row)
