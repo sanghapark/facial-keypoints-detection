e502bfe3b2c8cd0229ef56c35ef1cb13c81d9270
sanghapark <sanghapark1021@gmail.com>
2017-11-17 19:45:44 +0900

    fix bug

--- K/utils/loss_history.py
+++ K/utils/loss_history.py
@@ -2,15 +2,16 @@ import pandas as pd
 from keras.callbacks import Callback
 
 class LossHistory(Callback):
+
     def on_training_begin(self, logs={}):
         self.losses = pd.DataFrame(columns=['loss', 'val_loss', 'rmse', 'val_rmse'])
 
     def on_batch_end(self, batch, logs=None):
         cols = ['loss', 'val_loss', 'rmse', 'val_rmse']
-        losses = [
+        metrics = [
             logs.get('loss'),
             logs.get('val_loss'),
             logs.get('rmse'),
             logs.get('val_rmse')
         ]
-        self.losses.append(pd.DataFrame(losses, columns=cols))
+        self.losses.append(pd.DataFrame(metrics, columns=cols))
