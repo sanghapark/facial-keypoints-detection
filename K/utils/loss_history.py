ce3963e0316be3a108acf2a31c0ac07c40839969
sanghapark <sanghapark1021@gmail.com>
2017-11-17 23:12:47 +0900

    다시 수정

--- K/utils/loss_history.py
+++ K/utils/loss_history.py
@@ -4,15 +4,14 @@ from keras.callbacks import Callback
 class LossHistory(Callback):
 
     def __init__(self):
-        print('LossHistory init')
         self.losses = pd.DataFrame(columns=['loss', 'val_loss', 'rmse', 'val_rmse'])
 
     def on_epoch_end(self, epoch, logs=None):
         cols = ['loss', 'val_loss', 'rmse', 'val_rmse']
-        metrics = [
+        metrics = [[
             logs.get('loss'),
             logs.get('val_loss'),
             logs.get('rmse'),
             logs.get('val_rmse')
-        ]
+        ]]
         self.losses.append(pd.DataFrame(metrics, columns=cols))
