f21755e82d9b3d15272b959a791cfa9276c73b10
sanghapark <sanghapark1021@gmail.com>
2017-11-17 14:25:53 +0900

    상수는 따로 파일에 모아놈

--- K/train.py
+++ K/train.py
@@ -1,74 +1,13 @@
 import os
 import datetime as dt
-from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense
-from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam
+from keras.optimizers import RMSprop, Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.layers.advanced_activations import PReLU, LeakyReLU
-import keras.backend as K
 from utils.load import load_train_data_and_split
 from utils.metric import rmse
 from utils.data_augment_generator import DataAugmentGenerator
 from utils.cnn import *
 from utils.constant import *
 
-# (7000, 9)
-COLS01 = [
-    'left_eye_center_x',            'left_eye_center_y',
-    'right_eye_center_x',           'right_eye_center_y',
-    'nose_tip_x',                   'nose_tip_y',
-    'mouth_center_bottom_lip_x',    'mouth_center_bottom_lip_y',
-    'Image'
-]
-FLIP_INDICES01 = [
-    (0, 2),
-    (1, 3)
-]
-
-# (2155, 23)
-COLS02 = [
-    'left_eye_inner_corner_x',      'left_eye_inner_corner_y',
-    'left_eye_outer_corner_x',      'left_eye_outer_corner_y',
-    'right_eye_inner_corner_x',     'right_eye_inner_corner_y',
-    'right_eye_outer_corner_x',     'right_eye_outer_corner_y',
-    'left_eyebrow_inner_end_x',     'left_eyebrow_inner_end_y',
-    'left_eyebrow_outer_end_x',     'left_eyebrow_outer_end_y',
-    'right_eyebrow_inner_end_x',    'right_eyebrow_inner_end_y',
-    'right_eyebrow_outer_end_x',    'right_eyebrow_outer_end_y',
-    'mouth_left_corner_x',          'mouth_left_corner_y',
-    'mouth_right_corner_x',         'mouth_right_corner_y',
-    'mouth_center_top_lip_x',       'mouth_center_top_lip_y',
-    'Image'
-]
-FLIP_INDICES02 = [
-    (0, 4),
-    (1, 5),
-    (2, 6),
-    (3, 7),
-    (8, 12),
-    (9, 13),
-    (10, 14),
-    (11, 15),
-    (16, 18),
-    (17, 19)
-]
-
-BATCH_SIZE = 128
-EPOCHS01 = 300
-EPOCHS02 = 900
-VALIDATION_RATIO = 0.2
-
-ACTIVATION = 'elu'
-LAST_ACTIVATION = 'tanh'
-
-FLIP = True
-ROTATE = True
-CONTRAST = True
-FLIP_RATIO = 0.5
-ROTATE_RATIO = 0.5
-CONTRAST_RATIO = 0.5
-
-METRICS = [rmse]
 
 def train(filepath, cols, flip_indices, optimizer, epochs):
     X_train, X_valid, Y_train, Y_valid = load_train_data_and_split(FILEPATH_TRAIN, cols, VALIDATION_RATIO)
@@ -85,7 +24,7 @@ def train(filepath, cols, flip_indices, optimizer, epochs):
                                   patience=50,
                                   verbose=0,
                                   mode='min')
-    model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=METRICS)
+    model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=[rmse])
     model.load_weights(filepath) if os.path.exists(filepath) else None
     generator = DataAugmentGenerator(X_train,
                                      Y_train,
@@ -115,14 +54,3 @@ for i in range(20):
     filepath = 'cnn2_dataset02_{:02}.h5'.format(i)
     optimizer = Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     train(filepath, COLS02, FLIP_INDICES02, optimizer, EPOCHS02)
-
-
-# def save_model():
-#     model = create_cnn2(8, 'elu', 'tanh')
-#     optimizer = RMSprop(0.001, 0.9, 1e-8, 0)
-#     model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=[rmse])
-#     model.save('cnn2_dataset01.model')
-
-#     model_yaml = model.to_yaml()
-#     with open("cnn2_dataset01.yaml", "w") as yaml_file:
-#         yaml_file.write(model_yaml)
--- K/utils/constant.py
+++ K/utils/constant.py
@@ -4,3 +4,59 @@ COLOR_CHANNEL = 1
 
 FILEPATH_TRAIN = '../data/training.csv'
 FILEPATH_TEST  = '../data/test.csv'
+
+
+COLS01 = [
+    'left_eye_center_x',            'left_eye_center_y',
+    'right_eye_center_x',           'right_eye_center_y',
+    'nose_tip_x',                   'nose_tip_y',
+    'mouth_center_bottom_lip_x',    'mouth_center_bottom_lip_y',
+    'Image'
+]
+FLIP_INDICES01 = [
+    (0, 2),
+    (1, 3)
+]
+
+# (2155, 23)
+COLS02 = [
+    'left_eye_inner_corner_x',      'left_eye_inner_corner_y',
+    'left_eye_outer_corner_x',      'left_eye_outer_corner_y',
+    'right_eye_inner_corner_x',     'right_eye_inner_corner_y',
+    'right_eye_outer_corner_x',     'right_eye_outer_corner_y',
+    'left_eyebrow_inner_end_x',     'left_eyebrow_inner_end_y',
+    'left_eyebrow_outer_end_x',     'left_eyebrow_outer_end_y',
+    'right_eyebrow_inner_end_x',    'right_eyebrow_inner_end_y',
+    'right_eyebrow_outer_end_x',    'right_eyebrow_outer_end_y',
+    'mouth_left_corner_x',          'mouth_left_corner_y',
+    'mouth_right_corner_x',         'mouth_right_corner_y',
+    'mouth_center_top_lip_x',       'mouth_center_top_lip_y',
+    'Image'
+]
+FLIP_INDICES02 = [
+    (0, 4),
+    (1, 5),
+    (2, 6),
+    (3, 7),
+    (8, 12),
+    (9, 13),
+    (10, 14),
+    (11, 15),
+    (16, 18),
+    (17, 19)
+]
+
+BATCH_SIZE = 128
+EPOCHS01 = 300
+EPOCHS02 = 900
+VALIDATION_RATIO = 0.2
+
+ACTIVATION = 'elu'
+LAST_ACTIVATION = 'tanh'
+
+FLIP = True
+ROTATE = True
+CONTRAST = True
+FLIP_RATIO = 0.5
+ROTATE_RATIO = 0.5
+CONTRAST_RATIO = 0.5
