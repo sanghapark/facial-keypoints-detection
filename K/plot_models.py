bd5c5c7355220672dee41b14f352f20d6ef5085d
sanghapark <sanghapark1021@gmail.com>
2017-11-17 13:42:40 +0900

    model 잘못 생성된거 다시 수정

--- K/plot_models.py
+++ K/plot_models.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.models import model_from_yaml
 
-def load_model_with_weights(model_name):
+def load_models_with_weights(model_name):
     path = './models/{}/weights'.format(model_name)
     weights = [join(path, w) for w in listdir(path) if isfile(join(path, w))]
     models = []
-    for w in weights:
+    for idx, w in enumerate(weights):
         model_path = './models/{}/{}.yaml'.format(model_name, model_name)
         yaml_file = open(model_path, 'r')
         loaded_model_yaml = yaml_file.read()
@@ -19,6 +19,5 @@ def load_model_with_weights(model_name):
         models.append(model)
     return models
 
-models01 = load_model_with_weights('cnn2_dataset01')
-
-print(len(models01))
+models01 = load_models_with_weights('cnn2_dataset01')
+models02 = load_models_with_weights('cnn2_dataset02')
