e0e2db70dd999081436637e91c4179309078f906
sanghapark <sanghapark1021@gmail.com>
2017-11-21 05:23:53 +0900

    변형없이:

--- K/train_dataset01.py
+++ K/train_dataset01.py
@@ -21,10 +21,10 @@ for i in range(50):
     # model = create_cnn2(8, 'he_uniform', ACTIVATION, LAST_ACTIVATION)
     model = create_cnn2(8, 'he_normal', 'relu', 'relu')
 
-    # optimizer = RMSprop(0.001, 0.9, 1e-8, 0)
+    optimizer = RMSprop(0.001, 0.9, 1e-8, 0)
     # optimizer = Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     # optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    optimizer = Nadam()
+    # optimizer = Nadam()
 
     model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=[rmse])
     if i == 0:
